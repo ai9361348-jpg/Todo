5cb8ed001cc0649ba59d7c9a609bdf0e6e9cef62
ai9361348 <ai9361348@gmail.com>
2026-03-02 20:30:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react"
+
+
+function App() {
+        
+
+  const [todoData , setTodoData] = useState("")
+   const [todolist , setTodoList] = useState([])
+  const handleAdd = () =>{
+    const arr = [...todolist]
+    if(todoData.trim()==="")return
+    arr.push({id : Math.floor(Math.random() * 9000) , data:todoData})
+    setTodoList(arr)
+    setTodoData("")
+    localStorage.setItem("todo" , JSON.stringify(arr))
+
+
+  }
+  const deleteTodo = (id)=>{
+    setTodoList(todolist.filter(todolist => todolist.id !== id))
+  }
+
+  return (
+      
+      <div className="flex justify-center items-center h-dvh">
+        
+    
+             <div className="flex justify-center w-fit p-4 min-h-[300px]  items-center  gap-3 flex-col border border-solid border-black">
+            <div className="flex gap-3">
+                <input value={todoData} className="border border-black text-3xl rounded-r-xl rounded-l-xl"  onChange={(e) =>  setTodoData(e.target.value)} type="text" />
+            <button className="border border-black text-2xl p-2 bg-slate-500 text-white rounded-xl" onClick={handleAdd}>Add</button>
+
+            </div>
+            <div >
+                <ol className="list-decimal ">
+                   
+                {
+                  todolist.map((item)=>(
+                     <li key={item.id}>
+                     <div className="flex gap-3 mt-4">
+                    
+
+                        <p className="text-2xl">{item.data}</p>
+                      <button className="bg-blue-700 p-1 rounded-xl text-white" >Edit</button>
+                      <button className="bg-red-600 p-1 rounded-xl text-white" 
+                     
+                       onClick={() => deleteTodo (item.id)}
+                    
+                       
+                      >Deleat</button>
+                     </div>
+                    </li>
+                  ))
+                    
+                  
+                }
+                  
+                   
+                </ol>
+            </div>
+        </div>
+   
+      </div>
+ 
+
+
+  )
+}
+
+export default App
